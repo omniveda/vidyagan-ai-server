b9184f466420b6f252352c0f8fb2c580b3801c53
upmanpransh <upmanprashun987@gmail.com>
2026-05-07 21:51:59 +0530

    updaste done 2

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: "http://localhost:8080",
-    origin: "https://vidyagan-ai-server.onrender.com",
+    origin: "https://vidyagan-ai-frontend.vercel.app",
     // origin: "https://vidyagan-ai.com",
     // origin: "https://seccouncil.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
